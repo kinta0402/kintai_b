14cfc2d7fe207f71cc1d9da31cbb693221185c6c
kinta0402 <goro0410@gmail.com>
2019-04-27 07:51:16 +0000

    エラーメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new #新規作成されたUserオブジェクトをインスタンス変数に代入します
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #　保存が成功した場合はここに記述した処理が実行される。
+    else
+      render 'new'
+    end
+  end
+  
+  
+  private
+  
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+
 end
