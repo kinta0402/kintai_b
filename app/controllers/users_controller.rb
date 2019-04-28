f92537fc32db04e8963a41bfb575f3d64b1cd442
kinta0402 <goro0410@gmail.com>
2019-04-28 09:19:10 +0000

    実践向けのデプロイ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #　保存が成功した場合はここに記述した処理が実行される。
+      flash[:success] = "ユーザーの新規作成に成功しました"
+      redirect_to @user
     else
       render 'new'
     end
