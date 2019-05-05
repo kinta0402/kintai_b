5815fb1ad7820d383df31a94d5e9ff3ee41dc844
kinta0402 <goro0410@gmail.com>
2019-05-05 10:12:38 +0000

    次はレイアウトをログイン＆～

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    if user && user.authenticate(params[:session][:password]) #⓵userのメールとパスワードが一致する場合 =  一致するユーザーが存在する場合
+      log_in user   # session[:user_id] = 1 → user_id = 1 の情報をブラウザに保存させる (sessionに代入された値はブラウザに保存される) 
+                    # session[:user_id] = user.id
+                    # ⓶その存在するユーザーのid!?を変数sessionに代入する事で、ログインユーザーの情報が保持され続ける
       redirect_to user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,21 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # 現在ログイン中のユーザーを返す(ただし、いる場合のみ)
+  def current_user #訳：現在のユーザー
+    if session[:user_id] #セッションに(ブラウザに保存された!?)ユーザーIDが存在するかどうか？(ログイン中のユーザー) →trueなら下へ、nilならﾒｿｯﾄﾞは終了しnilを返す
+      @current_user ||= User.find_by(id: session[:user_id])
+                      #  ↓            ↓            ↓
+                      #usersﾃｰﾌﾞﾙの  idｶﾗﾑから  ﾛｸﾞｲﾝ中のﾕｰｻﾞｰidと等しいidを取得する
+    end
+  end
+  
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+    #否定演算子!を使ってる(trueならfalseに、falseならtrueになる)
+    #訳)ﾛｸﾞｲﾝ中のﾕｰｻﾞｰは存在しませんか?
+    #存在しない=trueがfalseに、存在する=falseがtrueに
+  end
 end
