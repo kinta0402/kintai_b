7d228278c86d535cccbabca34b039b29bce3e3f5
kinta0402 <goro0410@gmail.com>
2019-05-10 10:00:36 +0000

    次は正しいユーザーであることを要求する見直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   # editアクションとupdateアクションが実行される直前のみ、logged_in_userを実行する
   
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @user = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -25,16 +31,17 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])  ※before_actionにてeditアクションが実行される前に@userを定義してる為、削除
   end
   
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) ※before_actionにてudpateアクションが実行される前に@userを定義してる為、削除
     if @user.update_attributes(user_params)
+          #trueの場合、属性をアップデートする = ユーザー情報を更新する
       flash[:success] = "ユーザー情報を更新しました"
       redirect_to @user
     else
-      render 'edit'
+      render 'edit' # falseの場合、編集ページを再度表示する
     end
   end
   
@@ -45,6 +52,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
 
+
     # beforeアクション
     
     # ログイン済みユーザーか確認
@@ -53,5 +61,13 @@ class UsersController < ApplicationController
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(pramas[:id])
+      redirect_to(root_url) unless current_user?(@user) #@userはcurrent_userですか？
+                                  #unless → 条件式が偽の場合、処理を実行
+                                  #@user(DBのid)と、current_user(sessionに保存されてる現在のﾕｰｻﾞｰ)が偽の場合？、トップへリダイレクトする  
     end
 end
