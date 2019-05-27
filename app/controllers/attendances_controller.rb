1bbecaf5ec5ce4e445348294803f0731e29267fd
kinta0402 <goro0410@gmail.com>
2019-05-27 06:05:24 +0000

    リファクタリングの前

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -13,4 +13,11 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @first_day = Date.parse(params[:date])
+    @last_day = @first_day.end_of_month
+    @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/edit-basic-info/:id', to: 'users#edit_basic_info', as: :basic_info
-  patch 'update-basic-info', to: 'users#update_basic_info'
+  patch 'update-basic-info',  to: 'users#update_basic_info'
+  get 'users/:id/attendances/:date/edit', to: 'attendances#edit', as: :edit_attendances #勤怠情報を編集するページ
+  patch 'users/:id/attendances/:date/update', to: 'attendances#update', as: :update_attendances #勤怠情報を更新するページ
   resources :users do
     resources :attendances, only: :create
   end
