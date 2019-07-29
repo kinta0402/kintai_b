d79b77fa5a2b11458a53877938e634c1398ac843
kinta0402 <goro0410@gmail.com>
2019-07-29 09:16:44 +0000

    herokuタイムゾーン変更して再度レビュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :attendances, dependent: :destroy #attendanceモデルと紐づけ →1対多
+    has_many :attendances, dependent: :destroy #attendanceモデルと紐づけ →1対多の関係
     
     # 「remember_token」という仮想の属性を作成します。
     attr_accessor :remember_token
